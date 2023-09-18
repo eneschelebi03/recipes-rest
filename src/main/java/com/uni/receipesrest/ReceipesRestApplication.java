49278dee3f74373823e6c13b04a0ada2f8daeef1
ShadowCode887 <liontiger851@gmail.com>
2023-09-19 01:06:19 +0300

    Project init

---
+++ src/main/java/com/uni/receipesrest/ReceipesRestApplication.java
@@ -0,0 +1,13 @@
+package com.uni.receipesrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceipesRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceipesRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uni/receipesrest/ReceipesRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uni.receipesrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceipesRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
